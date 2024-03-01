e715cce12da192a212e170bfc8262f1923820fd9
Kenny <126126972+exile1217@users.noreply.github.com>
2024-02-29 23:52:19 -0500

    Location Access js code

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ function sayHello(){
     alert("Hello!");
 }
 
+const successCallback = (position) => {
+    console.log(position);
+  };
+  
+  const errorCallback = (error) => {
+    console.log(error);
+  };
+  
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  
